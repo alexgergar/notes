8b8f67098a631b9e0ebf5a0443f9253f11ef0a9b
alexgergar <alexandra.gergar@gmail.com>
2019-10-03 17:00:53 -0400

    Added axios with yarn and added a get request for lists

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav.js';
 import List from './components/List.js';
 import Note from './components/Note.js';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
@@ -17,13 +18,20 @@ class App extends Component {
       showNote: ! this.state.showNote // flips the state boolean
     })
   }
+
+  getNotes = () => {
+    axios.get('https://note-api-alexandra-gergar.herokuapp.com/notes')
+    .then((res) => console.log(res.data) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
   render() {
     const { showNote } = this.state;  
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
-          { showNote ? <Note /> : <List /> } 
+          { showNote ? <Note /> : <List getNotes={this.getNotes} /> } 
       </div>
     )
   }
